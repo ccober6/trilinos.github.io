196a652b90357b86a17dd0a9fbb9ef5a3b1c33ad
ccober6 <19675338+ccober6@users.noreply.github.com>
2026-09-20 04:53:09 +0000

    Update Doxygen

---
+++ docs/minitensor/navtreedata.js
@@ -0,0 +1,79 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "MiniTensor", "index.html", [
+    [ "Table of Contents", "index.html#minitensor_index", null ],
+    [ "MiniTensor Overview", "index.html#minitensor_overview", null ],
+    [ "Modules", "index.html#minitensor_modules", null ],
+    [ "Basic Usage", "index.html#minitensor_usage", null ],
+    [ "Copyright and License", "index.html#minitensor_copyright", null ],
+    [ "For All Questions and Comments...", "index.html#minitensor_questions", null ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_mini_tensor_8h.html",
+"functions_func_f.html",
+"group__minitensor__containers.html#ga63400178704348382370bceb9b92b818",
+"group__minitensor__containers.html#gaf82bf76bf55d84d3a54cbf1317de38ae",
+"group__minitensor__solvers.html#ga093a37638133383afeabb1bed756761b",
+"struct_sacado_1_1_is_equal_3_01_matrix_3_01_t_00_01_m_00_01_n_01_4_01_4.html",
+"structminitensor_1_1_newton_with_line_search_step.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
--- docs/teko/search/classes_9.js
+++ docs/teko/search/classes_9.js
@@ -1,11 +1,11 @@
 var searchData=
 [
-  ['mappingstrategy_0',['mappingstrategy',['../class_teko_1_1_epetra_1_1_mapping_strategy.html',1,'Teko::Epetra::MappingStrategy'],['../class_teko_1_1_tpetra_helpers_1_1_mapping_strategy.html',1,'Teko::TpetraHelpers::MappingStrategy']]],
-  ['mlpreconditionerfactory_1',['MLPreconditionerFactory',['../class_teko_1_1_m_l_preconditioner_factory.html',1,'Teko']]],
-  ['mlpreconditionerstate_2',['MLPreconditionerState',['../class_teko_1_1_m_l_preconditioner_state.html',1,'Teko']]],
-  ['modalpreconditionerfactory_3',['ModALPreconditionerFactory',['../class_teko_1_1_n_s_1_1_mod_a_l_preconditioner_factory.html',1,'Teko::NS']]],
-  ['modalprecondstate_4',['ModALPrecondState',['../class_teko_1_1_n_s_1_1_mod_a_l_precond_state.html',1,'Teko::NS']]],
-  ['multpreconditionerfactory_5',['MultPreconditionerFactory',['../class_teko_1_1_mult_preconditioner_factory.html',1,'Teko']]],
-  ['multprecondstate_6',['MultPrecondState',['../class_teko_1_1_mult_precond_state.html',1,'Teko']]],
-  ['multprecslinearop_7',['MultPrecsLinearOp',['../class_teko_1_1_mult_precs_linear_op.html',1,'Teko']]]
+  ['lscpreconditionerfactory_0',['LSCPreconditionerFactory',['../class_teko_1_1_n_s_1_1_l_s_c_preconditioner_factory.html',1,'Teko::NS']]],
+  ['lscprecondstate_1',['LSCPrecondState',['../class_teko_1_1_n_s_1_1_l_s_c_precond_state.html',1,'Teko::NS']]],
+  ['lscsimplecstrategy_2',['LSCSIMPLECStrategy',['../class_teko_1_1_n_s_1_1_l_s_c_s_i_m_p_l_e_c_strategy.html',1,'Teko::NS']]],
+  ['lscstrategy_3',['LSCStrategy',['../class_teko_1_1_n_s_1_1_l_s_c_strategy.html',1,'Teko::NS']]],
+  ['lu2x2diagonalstrategy_4',['LU2x2DiagonalStrategy',['../class_teko_1_1_l_u2x2_diagonal_strategy.html',1,'Teko']]],
+  ['lu2x2inverseop_5',['LU2x2InverseOp',['../class_teko_1_1_l_u2x2_inverse_op.html',1,'Teko']]],
+  ['lu2x2preconditionerfactory_6',['LU2x2PreconditionerFactory',['../class_teko_1_1_l_u2x2_preconditioner_factory.html',1,'Teko']]],
+  ['lu2x2strategy_7',['LU2x2Strategy',['../class_teko_1_1_l_u2x2_strategy.html',1,'Teko']]]
 ];
